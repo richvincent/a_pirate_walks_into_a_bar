de35abd7a758f8a6dbf58a3ea475dfdd66ed55e6
Richard Vincent <rich@richardvincent.com>
2015-12-24 17:09:57 +0000

    Still not working but my assertions work? wtf!

--- solution.py
+++ solution.py
@@ -1,20 +1,20 @@
 def answer(numbers):
-    if isinstance(numbers, list):
-        loop = list(set(numbers))
-        if len(loop) < 5000:
-            if len(loop) > 2:
-                if loop[0] == 0:
-                    return len(loop) - 1
-            elif len(loop) == 2:
-                return 2
+        if all(type(i) is int for i in numbers) is True:    
+            if isinstance(numbers, list):
+                loop = list(set(numbers))
+                if len(loop) < 5000:
+                    if len(loop) > 2:
+                        if loop[0] == 0:
+                            return len(loop) - 1
+                    elif len(loop) == 2:
+                        return 2
+                else:
+                    raise ValueError('Please no less than 2 and\
+                    no more than 5000 pirates')
             else:
-                raise ValueError('Please no less than 2 and\
-                no more than 5000 pirates')
+                raise ValueError('Please supply a list')
         else:
-            raise ValueError('Please supply a list')
-    else:
-        raise ValueError("Please ensure that only int's are\
-        in the list")
+            raise ValueError("Please ensure that only int's are in the list")
 
 
 assert (answer([1, 3, 0, 1]) == 2)
